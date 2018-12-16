0dcb3d26b47f1319e5a6a104104f01f514c894b0
Andreas <Andreas@LAPTOP-4CN35NHI.fritz.box>
2018-12-16 16:27:45 +0100

    Start

--- chatApplication/src/main/java/auditLog/AuditLogServer.java
+++ chatApplication/src/main/java/auditLog/AuditLogServer.java
@@ -8,12 +8,13 @@ import java.io.InputStreamReader;
 
 public class AuditLogServer {
 	private int port;
+	private ServerSocket serverSocket;
 	
 	public AuditLogServer(int port) {
 		this.port = port;
 	}
 	
-	public void startListening() {
+	public void startListening() throws IOException {
 		new Thread(new Runnable() {
 			
 			public void run() {
@@ -32,9 +33,10 @@ public class AuditLogServer {
 						if(s.hasNextLine()) {
 							System.out.println("[AUDIT] Neue Nachricht von der GUI \n" + s.nextLine());
 						}
+						serverSocket.close();
 						s.close();
 						remoteClientSocket.close();
-						serverSocket.close();
+						
 					}catch(Exception e) {
 						e.printStackTrace();
 						System.exit(1);
@@ -43,6 +45,7 @@ public class AuditLogServer {
 			}
 		}).start();
 	}
+	
 		
 		public static void main (String[]args)throws IOException{
 		AuditLogServer server = new AuditLogServer(50001);
